bdf7b94715a183f79d79d95a7fd85598499e27a4
Camilla Landin <landin.camilla8@gmail.com>
2024-05-17 18:06:24 +0200

    get A movie api req

--- src/service/filmService/swAPI.films.ts
+++ src/service/filmService/swAPI.films.ts
@@ -1,12 +1,22 @@
 import axios from "axios";
+import { FilmResponse, SingleFilm } from "./swAPI.filmTypes";
 
-const BASE_URL = "https://swapi.thehiveresistance.com/api/";
+const BASE_URL = "https://swapi.thehiveresistance.com/api";
 
 /**
  * Get all films
  */
 
 export const getMovies = async () => {
-    const res = await axios.get(BASE_URL + "films");
+    const res = await axios.get<FilmResponse>(BASE_URL + "/films");
     return res.data;
+}
+
+/**
+ * Get A Film
+ */
+
+export const getAMovie =async (filmId:  number) => {
+	const res = await axios.get<SingleFilm>(`${BASE_URL}/films/${filmId}`);
+	return res.data;
 }
