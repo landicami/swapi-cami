b73c20bd57a9a7c18c0eff33d802ad80855b5f8a
Camilla Landin <landin.camilla8@gmail.com>
2024-05-17 18:05:29 +0200

    Films/:id route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import "./assets/scss/App.scss";
 import { Routes, Route } from "react-router-dom";
-import  Container  from "react-bootstrap/Container";
+import Container from "react-bootstrap/Container";
 import HomePage from "./pages/HomePage";
 import Navigation from "./components/Navigation";
+import AFilmPage from "./pages/AFilmPage";
 import FilmPage from "./pages/FilmPage";
 import CharachterPage from "./pages/CharachterPage";
+import "./assets/scss/App.scss";
+
 
 function App() {
 
@@ -16,6 +18,7 @@ function App() {
           <Routes>
               <Route path="/" element={<HomePage />} />
               <Route path="/films" element={<FilmPage />} />
+			  <Route path="/films/:id" element={<AFilmPage />} />
               <Route path="/charachters" element={<CharachterPage />} />
           </Routes>
         </Container>
