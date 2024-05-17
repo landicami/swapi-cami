7804c61fbaf75f50fce2364e6b5cb73c2473d429
Camilla Landin <landin.camilla8@gmail.com>
2024-05-17 15:40:20 +0200

    adding links to pages

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,15 @@
-import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <div className='container d-flex justify-content-center align-items-center centered-container m-4'>
+    <div className='container d-flex justify-content-center align-items-center centered-container m-4 background'>
         <div className='bg-dark col-10 text-center p-3'>
             <h1 className="font-starwars">Home is where your heart is</h1>
             <p className="font-starwars">You have been requested to search the galaxy for stuff. Let's do it!</p>
-            <Button variant="outline-warning">Search</Button>
+			<Link to="/films" className='btn btn-warning me-2' role='button'>Search for films</Link>
+			<Link to="/charachters" className='btn btn-warning' role='button'>Search for charachters</Link>
+
+
         </div>
     </div>
   )
