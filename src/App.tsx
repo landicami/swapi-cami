2b08b61cfafc8014e8c75146c84d88a481317a66
Camilla Landin <landin.camilla8@gmail.com>
2024-05-21 19:29:46 +0200

    route for aCharachter and notfoundpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import Container from "react-bootstrap/Container";
+
 import HomePage from "./pages/HomePage";
 import Navigation from "./components/Navigation";
-import AFilmPage from "./pages/AFilmPage";
-import FilmPage from "./pages/FilmPage";
-import CharachterPage from "./pages/CharachterPage";
+
+import AFilmPage from "./pages/Filmpages/AFilmPage";
+import FilmPage from "./pages/Filmpages/FilmPage";
+
+import CharachterPage from "./pages/Charachterpages/CharachterPage";
+import ACharachterPage from "./pages/Charachterpages/ACharachterPage";
 import "./assets/scss/App.scss";
+import NotFoundpage from "./pages/NotFoundpage";
+
 
 
 function App() {
@@ -17,9 +23,17 @@ function App() {
         <Container>
           <Routes>
               <Route path="/" element={<HomePage />} />
+
               <Route path="/films" element={<FilmPage />} />
 			  <Route path="/films/:id" element={<AFilmPage />} />
+
               <Route path="/charachters" element={<CharachterPage />} />
+			  <Route path="/charachters/:id" element={<ACharachterPage />} />
+
+			  <Route path="*" element={<NotFoundpage />} />
+
+
+
           </Routes>
         </Container>
     </Container>
