cc05e5bfd62d2c11646989f3813ae86d305d468e
Camilla Landin <landin.camilla8@gmail.com>
2024-05-21 19:31:17 +0200

    api req for charachter

--- src/service/charService/swAPI.char.ts
+++ src/service/charService/swAPI.char.ts
@@ -1,5 +1,13 @@
 import axios from "axios";
+import { CharactherResponse } from "./swAPI.chartypes";
+
+const BASE_URL = "https://swapi.thehiveresistance.com/api"
 
 /**
  * Get all charachters
  */
+
+export const getPeople = async () => {
+	const res = await axios.get<CharactherResponse>(BASE_URL + "/people");
+	return res.data;
+}
