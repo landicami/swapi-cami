cacf27cef01dd2baf5851fafc3ac9c852ef1314a
Camilla Landin <landin.camilla8@gmail.com>
2024-05-21 10:51:21 +0200

    Completing all info about a film

--- src/pages/AFilmPage.tsx
+++ src/pages/AFilmPage.tsx
@@ -5,6 +5,8 @@ import { SingleFilm } from '../service/filmService/swAPI.filmTypes';
 import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
 import ListGroup from 'react-bootstrap/ListGroup';
+import Image from 'react-bootstrap/Image';
+import { ListGroupItem } from 'react-bootstrap';
 
 const AFilmPage = () => {
 	const [film, setFilm] = useState<SingleFilm | null>(null);
@@ -37,8 +39,15 @@ const AFilmPage = () => {
 	}, [])
 
   return (
-	<>
-	    	<h1 className='font-starwars'>Film</h1>
+	<Container className='m-auto'>
+			<div className='row'>
+				<div className='col 6'>
+					<h1 className='font-starwars'>Film</h1>
+				</div>
+				<div className='col-2'>
+					<Link to={"/films"} className='btn btn-warning mt-4' role='button'>Back to films</Link>
+				</div>
+			</div>
 
 			{error && (
 				<Container className='bg-dark'>
@@ -54,28 +63,92 @@ const AFilmPage = () => {
 
 			{film && (
 
-				<Card className='bg-dark row rounded col-12'>
+				<Card className='bg-dark row rounded col-10 p-2'>
 					<Card.Body>
-						<Card.Title className='font-starwars'>{film.title} {film.episode_id}</Card.Title>
-						<Card.Text>
-						{film.opening_crawl}
-						</Card.Text>
+						<Card.Title className='font-starwars'>{film.title}</Card.Title>
+                    		<Card.Text className="flex-grow-1">
+                        		{film.opening_crawl}
+                   			 </Card.Text>
 					</Card.Body>
+
 					<ListGroup className="list-group-flush">
+
+						<ListGroup.Item>Episode:  {film.episode_id}</ListGroup.Item>
 						<ListGroup.Item>Director: {film.director}</ListGroup.Item>
 						<ListGroup.Item>Producer: {film.producer}</ListGroup.Item>
-						<ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-					</ListGroup>
-					<Card.Body>
-						<Card.Link href="#">Card Link</Card.Link>
-						<Card.Link href="#">Another Link</Card.Link>
-					</Card.Body>
-					</Card>
+						<ListGroup.Item>Release date: {film.release_date}</ListGroup.Item>
 
+					</ListGroup>
+
+						<h5 className='font-starwars mt-4'>
+							Charachters in {film.title}:
+						</h5>
+						{film.characters.map(charachter =>
+						<Link to={`/people/${charachter.id}`}>
+							<ListGroup>
+								<ListGroup.Item className='link-card'>
+									{charachter.name}
+								</ListGroup.Item>
+							</ListGroup>
+							</Link>
+						)}
+
+						<h5 className='font-starwars mt-4'>
+							Planets in {film.title}:
+						</h5>
+						{film.planets.map(planet =>
+						// <Link to={`/planet/${planet.id}`}>
+							<ListGroup>
+								<ListGroup.Item>
+									{planet.name}
+								</ListGroup.Item>
+							</ListGroup>
+							// </Link>
+						)}
+
+						<h5 className='font-starwars mt-4'>
+							Starships in {film.title}:
+						</h5>
+						{film.starships.map(ships =>
+						// <Link to={`/starships/${ships.id}`}>
+							<ListGroup>
+								<ListGroup.Item>
+									{ships.name}
+								</ListGroup.Item>
+							</ListGroup>
+						)}
+
+						<h5 className='font-starwars mt-4'>
+							Vehicles in {film.title}:
+						</h5>
+						{film.vehicles.map(vehicle =>
+						// <Link to={`/vehicles/${vehicle.id}`}>
+							<ListGroup>
+								<ListGroup.Item>
+									{vehicle.name}
+								</ListGroup.Item>
+							</ListGroup>
+							// </Link>
+						)}
+
+						<h5 className='font-starwars mt-4'>
+							Species in {film.title}:
+						</h5>
+						{film.species.map(specie =>
+						// <Link to={`/vehicles/${vehicle.id}`}>
+							<ListGroup>
+								<ListGroup.Item>
+									{specie.name}
+								</ListGroup.Item>
+							</ListGroup>
+							// </Link>
+						)}
+					</Card>
 			)}
 
-	<Link to={"/films"} className='btn btn-warning' role='button'>Back to films</Link>
-	</>
+
+
+	</Container>
   )
 }
 
