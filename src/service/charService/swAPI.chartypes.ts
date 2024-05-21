40a8f8ee32fd19df9a579e347960387d1d18c41e
Camilla Landin <landin.camilla8@gmail.com>
2024-05-21 19:31:24 +0200

    types for api req

--- src/service/charService/swAPI.chartypes.ts
+++ src/service/charService/swAPI.chartypes.ts
@@ -1,3 +1,45 @@
-export interface CharactherType {
-    
-}
+export interface CharactherResponse {
+		current_page: number;
+		data: Charachter[];
+		first_page_url: string;
+		from: number;
+		last_page: number;
+		last_page_url: string;
+		links: Links[];
+		next_page_url: string;
+		per_page: number;
+		prev_page_ur: null | string;
+		to: number;
+		total: number;
+	}
+
+
+	interface Links {
+		url: null | string;
+		label: string;
+		active: boolean;
+
+	}
+	interface Charachter {
+		id: number;
+		name: string,
+		birth_year: string;
+		eye_color: string;
+		hair_color: string;
+		height: string;
+		mass: string;
+		skin_color: string;
+		wiki_link: string;
+		image_url: string;
+		affiliations: string[];
+		created: string;
+		edited: string;
+		films_count: number;
+		species_count: number;
+		starships_count: number;
+		vehicles_count: number;
+		homeworld: {
+			id: number;
+			name: string;
+		},
+	}
