8e0ce18203e7c1a05eed348b2598fa0baa1e3486
Camilla Landin <landin.camilla8@gmail.com>
2024-05-28 11:47:25 +0200

    getting pagination comp work

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,32 +1,55 @@
 import React from 'react'
-import { CharactherResponse } from '../service/charService/swAPI.chartypes'
-
 import Button from "react-bootstrap/Button";
+import { CharactherResponse } from '../service/charService/swAPI.chartypes'
+import { FilmResponse } from '../service/filmService/swAPI.filmTypes';
 
-interface PaginationProps {
-	data: CharactherResponse
-	getAllResources: (url?: string) => Promise<void>;
+interface PaginationProps{
+	hasNextPage: boolean
+	hasPreviousPage: boolean;
+
+	data: CharactherResponse | FilmResponse;
+	onNextPage: () => void;
+	onPreviousPage: () => void;
+
 }
 
-const Pagination :React.FC<PaginationProps> = ({ data, getAllResources }) => {
+const Pagination: React.FC<PaginationProps> = ({
+	data,
+	hasPreviousPage,
+	hasNextPage,
+	onNextPage,
+	onPreviousPage
+	}) => {
   return (
-	<div className="d-flex justify-content-center">
-                {data.links.map((link, index) => (
-                 <>
-				   <Button
-                        key={index}
-                        variant={link.active ? "secondary" : "warning"}
-                        disabled={link.url === null}
-                        onClick={() => {link.url && getAllResources(link.url )}}
-                        className='me-2'
-                    >
-						{link.label}
-                    </Button>
 
-					</>
-                ))}
+		<div className='row'>
+			<div className='d-flex justify-content-around'>
+				<div>
+				<Button
+				variant="warning"
+				onClick={onPreviousPage}
+				disabled={!hasPreviousPage}
+				>
+					&laquo; Previous
+				</Button>
+				</div>
+				<div className='bg-dark mb-0 p-2 rounded'>
+					<p className='m-0 font-starwars'>
+						{data.current_page} of {data.last_page}
+					</p>
+				</div>
+				<div>
+					<Button
+					variant="warning"
+					onClick={onNextPage}
+					disabled={!hasNextPage}
+					>
+					Next &raquo;</Button>
+				</div>
+			</div>
 		</div>
+
   )
 }
 
-export default Pagination
+export default Pagination;
