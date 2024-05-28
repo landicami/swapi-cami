5609c133ab14c341489c5ddf8f001024fbbb17a5
Camilla Landin <landin.camilla8@gmail.com>
2024-05-28 13:54:14 +0200

    Dropdown for searchqueries

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,22 @@
 import { Link } from "react-router-dom";
+import  Dropdown  from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
 
 const HomePage = () => {
   return (
-    <div className='container d-flex justify-content-center align-items-center centered-container m-4 background'>
+    <div className='container d-flex justify-content-center align-items-center m-4'>
         <div className='bg-dark col-10 text-center p-3 border border-warning rounded'>
             <h1 className="font-starwars">Beep Boop!</h1>
             <h2 className="font-starwars">You have been requested to search the galaxy for stuff.</h2>
 			<h3 className="font-starwars"> Let's do it!</h3>
-			<Link to="/films" className='btn btn-warning me-2' role='button'>Search for films</Link>
-			<Link to="/people" className='btn btn-warning' role='button'>Search for charachters</Link>
+
+			<DropdownButton id="dropdown-basic-button" title="Choose search" variant="warning">
+				<Dropdown.Item as={Link} to="/films">Search for films</Dropdown.Item>
+				<Dropdown.Item as={Link} to="/people">Search for characters</Dropdown.Item>
+				<Dropdown.Item as={Link} to="/planets">Search for planets</Dropdown.Item>
+				<Dropdown.Item as={Link} to="/species">Search for species</Dropdown.Item>
+				<Dropdown.Item as={Link} to="/vehicles">Search for vehicles</Dropdown.Item>
+   			 </DropdownButton>
 
 
         </div>
