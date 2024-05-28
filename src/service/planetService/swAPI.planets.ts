fcf5ca47f83183893a23732fdd3b1b2d37a9bb96
Camilla Landin <landin.camilla8@gmail.com>
2024-05-28 15:24:15 +0200

    getplanets from api

--- src/service/planetService/swAPI.planets.ts
+++ src/service/planetService/swAPI.planets.ts
@@ -7,8 +7,8 @@ const BASE_URL = "https://swapi.thehiveresistance.com/api";
  * Get all planets
  */
 
-export const getMovies = async () => {
-    const res = await axios.get<PlanetResponse>(BASE_URL + "/planets");
+export const getPlanets = async (page: number) => {
+    const res = await axios.get<PlanetResponse>(`${BASE_URL}/planets/?page=${page}`);
     return res.data;
 }
 
@@ -16,7 +16,7 @@ export const getMovies = async () => {
  * Get A Planet
  */
 
-export const getAMovie = async (planetId:  number) => {
+export const getAPlanet = async (planetId:  number) => {
 	const res = await axios.get<APlanet>(`${BASE_URL}/planets/${planetId}`);
 	return res.data;
 }
@@ -26,7 +26,7 @@ export const getAMovie = async (planetId:  number) => {
 Search for a planet
 */
 
-export const searchAMovie = async (galaxySearch : string) => {
-	const res = await axios.get<PlanetResponse>(`${BASE_URL}/planets/?search=${galaxySearch}`);
+export const searchAPlanet = async (galaxySearch : string, page: number) => {
+	const res = await axios.get<PlanetResponse>(`${BASE_URL}/planets/?search=${galaxySearch}&page=${page}`);
 	return res.data
 }
