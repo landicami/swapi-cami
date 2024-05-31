8b2b59dcd4cf3d6f06c5575cf4afc613b89ec977
Camilla Landin <landin.camilla8@gmail.com>
2024-05-31 12:03:16 +0200

    useNavigate -1

--- src/pages/NotFoundpage.tsx
+++ src/pages/NotFoundpage.tsx
@@ -1,13 +1,21 @@
+import Button from "react-bootstrap/Button"
 import Container from 'react-bootstrap/Container'
+import { useNavigate } from 'react-router-dom'
+
 
 const NotFoundpage = () => {
+	const navigate = useNavigate();
   return (
 	<Container>
 		<div className='d-flex justify-content-center align-items-center vh-100'>
-			<div className='text-center font-starwars'>
-				<h1>🛑 Not exists, does page. 🛑 <br /></h1>
-				<h2>Try, you will again. Fail, will you?</h2>
+			<div className='text-center'>
+				<h1 className=" font-starwars">🛑 Not exists, does page. 🛑 <br /></h1>
+				<h2 className=" font-starwars">Try, you will again. Fail, will you?</h2>
+					<Button  className="mt-2" variant="warning" onClick={() => navigate(-1)}>
+						Wanna go back?
+					</Button>
 			</div>
+
 		</div>
 	</Container>
   )
